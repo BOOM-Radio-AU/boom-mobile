c0952d6324ca9116e36c701a3113c916019ec618
David Stern <20023188@tafe.wa.edu.au>
2021-09-08 13:41:14 +0800

    Slide-out menu; navigate to views from menu

--- BoomRadio/BoomRadio/MainPage.xaml.cs
+++ BoomRadio/BoomRadio/MainPage.xaml.cs
@@ -15,22 +15,36 @@ namespace BoomRadio
 
         Dictionary<string, StackLayout> Views = new Dictionary<string, StackLayout>();
         string CurrentView;
+        bool MenuShown = false;
 
         public MainPage()
         {
             InitializeComponent();
             On<Xamarin.Forms.PlatformConfiguration.iOS>().SetUseSafeArea(true);
+            // Initialise views to load into content area
             Views["home"] = new HomeView();
             Views["shows"] = new ShowsView();
             Views["news"] = new NewsView();
+            Views["about"] = new AboutView();
+            Views["contact"] = new ContactView();
+            Views["settings"] = new SettingsView();
             CurrentView = "home";
+            UpdateUI();
         }
 
+        /// <summary>
+        /// Loads a view into the content area of the main page
+        /// </summary>
+        /// <param name="target">Name of view to load</param>
         public void Navigate(string target)
         {
             if (!Views.ContainsKey(target))
             {
                 throw new Exception($"[Navigation error] View not found for '{target}'");
+            }
+            if (MenuShown)
+            {
+                CloseMenu();
             }
             ContentAreaScrollView.Content = Views[target];
             CurrentView = target;
@@ -43,7 +57,6 @@ namespace BoomRadio
             HomeTab.TextColor = CurrentView == "home" ? Color.Orange : Color.Black;
             ShowsTab.TextColor = CurrentView == "shows" ? Color.Orange : Color.Black;
             NewsTab.TextColor = CurrentView == "news" ? Color.Orange : Color.Black;
-
         }
 
         private void HomeTab_Clicked(object sender, EventArgs e)
@@ -59,6 +72,55 @@ namespace BoomRadio
         private void NewsTab_Clicked(object sender, EventArgs e)
         {
             Navigate("news");
+        }
+
+        private void OpenMenu()
+        {
+            if (!MenuShown)
+            {
+                MenuFrame.TranslateTo(0, 0, 400, Easing.Linear);
+                MenuShown = true;
+            }
+        }
+        private void CloseMenu()
+        {
+            if (MenuShown)
+            {
+                MenuFrame.TranslateTo(-200, 0, 200, Easing.Linear);
+                MenuShown = false;
+            }
+        }
+
+        private void Menu_Swiped_Left(object sender, SwipedEventArgs e)
+        {
+            CloseMenu();
+        }
+
+        private void MenuIcon_Tapped(object sender, EventArgs e)
+        {
+            if (MenuShown)
+            {
+                CloseMenu();
+            }
+            else
+            {
+                OpenMenu();
+            }
+        }
+
+        private void AboutMenuItem_Clicked(object sender, EventArgs e)
+        {
+            Navigate("about");
+        }
+
+        private void ContactMenuItem_Clicked(object sender, EventArgs e)
+        {
+            Navigate("contact");
+        }
+
+        private void SettingsMenuItem_Clicked(object sender, EventArgs e)
+        {
+            Navigate("settings");
         }
     }
 }
