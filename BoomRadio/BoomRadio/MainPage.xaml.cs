8ef8fa054f2c20ee7727e7e931fdaf6aefe0f6c8
David Stern <20023188@tafe.wa.edu.au>
2021-09-08 10:13:07 +0800

    Show content from separate page in content area

--- BoomRadio/BoomRadio/MainPage.xaml.cs
+++ BoomRadio/BoomRadio/MainPage.xaml.cs
@@ -13,67 +13,67 @@ namespace BoomRadio
     {
 
 
-        bool IsPlaying;
-        string coverImageUri;
-        Track trackInfo;
+        //bool IsPlaying;
+        //string coverImageUri;
+        //Track trackInfo;
 
         public MainPage()
         {
             InitializeComponent();
             On<Xamarin.Forms.PlatformConfiguration.iOS>().SetUseSafeArea(true);
-            IsPlaying = false;
-            trackInfo = new Track();
-            StatusLabel.Text = "Not yet playing";
+            //IsPlaying = false;
+            //trackInfo = new Track();
+            //StatusLabel.Text = "Not yet playing";
         }
         private void UpdateUI()
         {
-            StatusLabel.Text = IsPlaying ? "Now playing" : "Stopped playing";
-            ArtistLabel.Text = trackInfo.Artist;
-            SongTitleLabel.Text = trackInfo.Title;
-            if (coverImageUri != trackInfo.ImageUri)
-            {
-                coverImageUri = trackInfo.ImageUri;
-                CoverImage.Source = ImageSource.FromUri(new Uri(coverImageUri));
-            }
+            //StatusLabel.Text = IsPlaying ? "Now playing" : "Stopped playing";
+            //ArtistLabel.Text = trackInfo.Artist;
+            //SongTitleLabel.Text = trackInfo.Title;
+            //if (coverImageUri != trackInfo.ImageUri)
+            //{
+            //    coverImageUri = trackInfo.ImageUri;
+            //    CoverImage.Source = ImageSource.FromUri(new Uri(coverImageUri));
+            //}
         }
-        private async void UpdateTrackInfo()
-        {
-            await trackInfo.Update();
-            Device.BeginInvokeOnMainThread(() => UpdateUI()); // The UI can only be updated from the main thread
-        }
+        //private async void UpdateTrackInfo()
+        //{
+        //    await trackInfo.Update();
+        //    Device.BeginInvokeOnMainThread(() => UpdateUI()); // The UI can only be updated from the main thread
+        //}
 
-        private void KeepTrackInfoUpdated()
-        {
-            UpdateTrackInfo();
-            Device.StartTimer(TimeSpan.FromSeconds(15), () =>
-            {
-                UpdateTrackInfo();
-                return IsPlaying;
-            });
-        }
+        //private void KeepTrackInfoUpdated()
+        //{
+        //    UpdateTrackInfo();
+        //    Device.StartTimer(TimeSpan.FromSeconds(15), () =>
+        //    {
+        //        UpdateTrackInfo();
+        //        return IsPlaying;
+        //    });
+        //}
 
-        private void PlayButton_Clicked(object sender, EventArgs e)
-        {
-            DependencyService.Get<IStreaming>().Play();
-            IsPlaying = true;
-            UpdateUI();
-            KeepTrackInfoUpdated();
-        }
+        //private void PlayButton_Clicked(object sender, EventArgs e)
+        //{
+        //    DependencyService.Get<IStreaming>().Play();
+        //    IsPlaying = true;
+        //    UpdateUI();
+        //    KeepTrackInfoUpdated();
+        //}
 
 
 
-        private void PauseButton_Clicked(object sender, EventArgs e)
-        {
-            DependencyService.Get<IStreaming>().Pause();
-            IsPlaying = false;
-            UpdateUI();
-        }
+        //private void PauseButton_Clicked(object sender, EventArgs e)
+        //{
+        //    DependencyService.Get<IStreaming>().Pause();
+        //    IsPlaying = false;
+        //    UpdateUI();
+        //}
 
-        private void StopButton_Clicked(object sender, EventArgs e)
-        {
-            DependencyService.Get<IStreaming>().Stop();
-            IsPlaying = false;
-            UpdateUI();
-        }
+        //private void StopButton_Clicked(object sender, EventArgs e)
+        //{
+        //    DependencyService.Get<IStreaming>().Stop();
+        //    IsPlaying = false;
+        //    UpdateUI();
+        //}
     }
 }
