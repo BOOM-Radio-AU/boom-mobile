ff517b765b6d8a1c8e313d553458ca96705f3c33
Tom Egerton <tom.egerton@hotmail.com>
2021-09-10 11:41:14 +0800

    Icons for menu and live

--- BoomRadio/BoomRadio/AssemblyInfo.cs
+++ BoomRadio/BoomRadio/AssemblyInfo.cs
@@ -1,3 +1,7 @@
 using Xamarin.Forms.Xaml;
+using Xamarin.Forms;
 
 [assembly: XamlCompilation(XamlCompilationOptions.Compile)]
+[assembly: ExportFont("Font Awesome 5 Brands.otf", Alias = "FA-B")]
+[assembly: ExportFont("Font Awesome 5 Regular.otf", Alias = "FA-R")]
+[assembly: ExportFont("Font Awesome 5 Solid.otf", Alias = "FA-S")]
