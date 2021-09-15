5d1ac7c407ad1779201ec6d732cd6343123ac372
Tom Egerton <tom.egerton@hotmail.com>
2021-09-15 10:44:10 +0800

    Added fonts and styling for the layout

--- BoomRadio/BoomRadio/AssemblyInfo.cs
+++ BoomRadio/BoomRadio/AssemblyInfo.cs
@@ -5,3 +5,6 @@ using Xamarin.Forms;
 [assembly: ExportFont("Font Awesome 5 Brands.otf", Alias = "FA-B")]
 [assembly: ExportFont("Font Awesome 5 Regular.otf", Alias = "FA-R")]
 [assembly: ExportFont("Font Awesome 5 Solid.otf", Alias = "FA-S")]
+[assembly: ExportFont("Cocogoose light.ttf", Alias = "CG-R")]
+[assembly: ExportFont("Cocogoose Bold.ttf", Alias = "CG-B")]
+[assembly: ExportFont("Cocogoose Italic.ttf", Alias = "CG-I")]
