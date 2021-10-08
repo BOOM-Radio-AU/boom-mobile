70423924be7f9572aa49edeb5e2a3e682bc36869
David Stern <20023188@tafe.wa.edu.au>
2021-10-08 09:40:48 +0800

    Show, store and update preferences in setting view
    Functionality related to each preference not yet implemented.

--- BoomRadio/BoomRadio/View/SettingsView.xaml.cs
+++ BoomRadio/BoomRadio/View/SettingsView.xaml.cs
@@ -1,20 +1,53 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-using Xamarin.Forms;
+﻿using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
+using Xamarin.Essentials;
 
 namespace BoomRadio.View
 {
     [XamlCompilation(XamlCompilationOptions.Compile)]
-    public partial class SettingsView : StackLayout
+    public partial class SettingsView : StackLayout, IUpdatableUI
     {
+        // Keys used to store preferences
+        readonly string darkModeKey = "darkMode";
+        readonly string deviceDarkModeKey = "deviceDarkMode";
+        readonly string autoplayKey = "autoplay";
+
         public SettingsView()
         {
             InitializeComponent();
         }
+
+        /// <summary>
+        /// Updates the user interface
+        /// </summary>
+        public void UpdateUI()
+        {
+            // Toggle switches to the saved preference values, or off by default
+            DarkModeSwitch.IsToggled = Preferences.Get(darkModeKey, false);
+            DeviceDarkModeSwitch.IsToggled = Preferences.Get(deviceDarkModeKey, false);
+            AutoplaySwitch.IsToggled = Preferences.Get(autoplayKey, false);
+        }
+
+        /// <summary>
+        /// Handles the dark mode switch being toggled
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void DarkModeSwitch_Toggled(object sender, ToggledEventArgs e) => Preferences.Set(darkModeKey, e.Value);
+
+        /// <summary>
+        /// Handles the device dark mode switch being toggled
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void DeviceDarkModeSwitch_Toggled(object sender, ToggledEventArgs e) => Preferences.Set(deviceDarkModeKey, e.Value);
+
+        /// <summary>
+        /// Handles the autoplay switch being toggled
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void AutoplaySwitch_Toggled(object sender, ToggledEventArgs e) => Preferences.Set(autoplayKey, e.Value);
+
     }
 }
