70bf27eed88c243b591d42795275a17671726a25
Tom Egerton <tom.egerton@hotmail.com>
2021-10-08 12:20:24 +0800

    Classes and views for shows

---
+++ BoomRadio/BoomRadio/Components/ShowFrame.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace BoomRadio.Components
+{
+    public partial class ShowFrame : Frame
+    {
+        public ShowFrame()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ BoomRadio/BoomRadio/Model/Shows.cs
@@ -0,0 +1,54 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+
+namespace BoomRadio.Model
+{
+    public class Shows
+    {
+
+
+        public int ID { get; set; }
+        public string ShowTitle { get; set; }
+        public string ShowSchedule { get; set; }
+        public string ShowDescription { get; set; }
+        public string ShowImage { get; set; }
+
+
+        private readonly HttpClient client = new HttpClient();
+        public string MediaApiPrefix = "https://boomradio.com.au/wp-json/wp/v2/schedule/";
+
+
+
+        public Shows(int id, string title, string time, string description, string imageURL)
+        {
+            ID = id;
+            ShowTitle = title;
+            ShowSchedule = time;
+            ShowDescription = description;
+            ShowImage = imageURL;
+        }
+
+
+
+        public string TextFromHTML(string html)
+        {
+            Regex stripFormattingRegex = new Regex(@"<[^>]*(>|$)", RegexOptions.Multiline);
+            string text = stripFormattingRegex.Replace(html, string.Empty);
+            text = System.Net.WebUtility.HtmlDecode(text);
+            return text.Trim();
+        }
+
+
+
+
+
+
+    }
+}
