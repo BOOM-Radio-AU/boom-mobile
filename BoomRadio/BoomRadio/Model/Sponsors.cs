51d15c1ead6bb1e5ea29e2398642412531437469
Tom Egerton <tom.egerton@hotmail.com>
2021-10-28 13:12:52 +0800

    About page expand boxes and sponsors framework. Added community toolbox nuget package

---
+++ BoomRadio/BoomRadio/Model/Sponsors.cs
@@ -0,0 +1,20 @@
+﻿using System;
+namespace BoomRadio.Model
+{
+    public class Sponsors
+    {
+
+
+        public int ID { get; set; }
+        public string SponsorName { get; set; }
+        public string SponsorDescription { get; set; }
+        public string SponsorImageQueryUrl { get; set; }
+        public string SponsorImageUrl { get; set; }
+
+        public Sponsors()
+        {
+
+        }
+        
+    }
+}
