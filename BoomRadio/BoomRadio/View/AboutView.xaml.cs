0371c9aae3a6925f7c3c614520daa1c6e9c7fb81
Tom Egerton <tom.egerton@hotmail.com>
2021-10-29 10:06:02 +0800

    created sponsors classes

--- BoomRadio/BoomRadio/View/AboutView.xaml.cs
+++ BoomRadio/BoomRadio/View/AboutView.xaml.cs
@@ -3,19 +3,31 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Collections.ObjectModel;
 
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 using Xamarin.CommunityToolkit;
+using BoomRadio.Model;
 
 namespace BoomRadio.View
 {
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class AboutView : StackLayout
     {
-        public AboutView()
+
+        public SponsorsCollection SponsC;
+        public ObservableCollection<Sponsors> Sponsor { get; set; } = new ObservableCollection<Sponsors>();
+
+        public AboutView(SponsorsCollection Sponsor)
         {
             InitializeComponent();
+            SponsC = Sponsor;
+            BindingContext = this;
+        }
+
+        public async void UpdateUI()
+        {
         }
     }
 }
