239d5eeeda79dc5b270b06d1bc26e8fd5fdd3342
David Stern <20023188@tafe.wa.edu.au>
2021-11-01 15:05:26 +0800

    Fix changing carousel colours when dark mode turns on/off

--- BoomRadio/BoomRadio/View/AboutView.xaml.cs
+++ BoomRadio/BoomRadio/View/AboutView.xaml.cs
@@ -19,6 +19,27 @@ namespace BoomRadio.View
         public SponsorsCollection SponsC;
         public ObservableCollection<Sponsors> Sponsor { get; set; } = new ObservableCollection<Sponsors>();
 
+        // Some colour fields and properties for data binding
+        private Color textColour = Theme.GetColour("text");
+        private Color bgColour = Theme.GetColour("background");
+        public Color TextColour
+        {
+            get => textColour; private set
+            {
+                textColour = value;
+                OnPropertyChanged("TextColour");
+            }
+        }
+        public Color BgColour
+        {
+            get => bgColour;
+            private set
+            {
+                bgColour = value;
+                OnPropertyChanged("BgColour");
+            }
+        }
+
         public AboutView(SponsorsCollection Sponsor)
         {
             InitializeComponent();
@@ -29,6 +50,9 @@ namespace BoomRadio.View
 
         public async void UpdateUI()
         {
+            // Update colours
+            TextColour = Theme.GetColour("text");
+            BgColour = Theme.GetColour("background");
 
             await SponsC.UpdateAsync();
 
